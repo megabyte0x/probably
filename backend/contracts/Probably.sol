2ef9b1ef46d8958149d364a3aded6e810e02d80b
megabyte0x <yashygkf@gmail.com>
2023-01-18 23:35:02 +0530

    init NFT contract and Main contract

--- backend/contracts/Probably.sol
+++ backend/contracts/Probably.sol
@@ -1,13 +1,49 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity 0.8.9;
 
-// Importing Context Smart Contract
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+
 import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract probably is Context {
+import "./ProbablyNFT.sol";
+
+contract probably is Context, Ownable, IERC721Receiver {
     address deployer;
 
-    constructor() {
+    ProbablyNFT probablyNFT;
+
+    constructor(
+        address vrfCoordinator,
+        address linkToken,
+        bytes32 vrfKeyHash,
+        uint256 vrfFee,
+        string memory _baseTokenURI
+    ) {
         deployer = _msgSender();
+        probablyNFT = new ProbablyNFT(
+            _baseTokenURI,
+            vrfCoordinator,
+            linkToken,
+            vrfKeyHash,
+            vrfFee
+        );
+    }
+
+    function buyTicket() public payable {
+        probablyNFT.mint();
+    }
+
+    function deleteTicket(uint256 _tokenId) public {
+        probablyNFT.burn(_tokenId);
+    }
+
+    function pauseGame(bool _val) public onlyOwner {
+        probablyNFT.setPaused(_val);
+    }
+
+    function withdrawFunds() public onlyOwner {
+        probablyNFT.withdraw();
     }
 }
