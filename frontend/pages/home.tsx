f92f4e2516b774761c353ee3f331dd14ceba2a77
megabyte0x <yashygkf@gmail.com>
2023-01-19 10:11:44 +0530

    tag line updated

--- frontend/pages/home.tsx
+++ frontend/pages/home.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <>
       <h1>WAGMI</h1>
+      <h1>You are now in the probability with probably.</h1>
       <ConnectButton></ConnectButton>
     </>
   );
