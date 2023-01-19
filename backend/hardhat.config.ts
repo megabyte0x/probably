237e550f7d86c82348ef9f2303359f8277a1ac3a
megabyte0x <yashygkf@gmail.com>
2023-01-19 10:44:39 +0530

    configured ts

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,11 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
 require("dotenv").config({ path: ".env" });
 
-const ALCHEMY_HTTP_URL = process.env.ALCHEMY_HTTP_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const POLYGON_SCAN_KEY = process.env.POLYGON_SCAN_KEY;
+const ALCHEMY_HTTP_URL = process.env.ALCHEMY_HTTP_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const POLYGON_SCAN_KEY = process.env.POLYGON_SCAN_KEY || "";
 
-module.exports = {
+const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
     mumbai: {
@@ -19,3 +21,5 @@ module.exports = {
     },
   },
 };
+
+export default config;
