a4c86e434e32f9cfe289af1b4f5e30f28f3715b1
megabyte0x <yashygkf@gmail.com>
2023-01-20 20:44:29 +0530

    implemented IERC721

--- backend/contracts/Probably.sol
+++ backend/contracts/Probably.sol
@@ -1,34 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.9;
 
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
-
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
 import "./ProbablyNFT.sol";
 
-contract Probably is Context, Ownable {
+contract Probably is Context, Ownable, IERC721Receiver {
     address deployer;
 
     ProbablyNFT probablyNFT;
 
     constructor(
-        address vrfCoordinator,
-        address linkToken,
-        bytes32 vrfKeyHash,
-        uint256 vrfFee,
-        string memory _baseTokenURI
-    ) {
+        // address vrfCoordinator,
+        // address linkToken,
+        // bytes32 vrfKeyHash,
+        // uint256 vrfFee,
+        address probablyNFTAddress
+    )
+        // string memory _baseTokenURI
+        IERC721Receiver()
+    {
+        setUp(probablyNFTAddress);
         deployer = _msgSender();
-        probablyNFT = new ProbablyNFT(
-            _baseTokenURI,
-            vrfCoordinator,
-            linkToken,
-            vrfKeyHash,
-            vrfFee
-        );
     }
+
+    function setUp(address nftAddress) internal {
+        probablyNFT = ProbablyNFT(payable(nftAddress));
+    }
+
+    // function getProbablyNFTAddress() public view returns (address) {
+    //     return address(probablyNFT);
+    // }
 
     function buyTicket() public payable {
         probablyNFT.mint();
@@ -44,5 +48,14 @@ contract Probably is Context, Ownable {
 
     function withdrawFunds() public onlyOwner {
         probablyNFT.withdraw();
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external pure returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
     }
 }
