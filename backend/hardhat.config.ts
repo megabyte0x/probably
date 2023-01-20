a24ae6a615c161731e46913d2d2f99b63749a7ab
megabyte0x <yashygkf@gmail.com>
2023-01-20 18:44:31 +0530

    corrected the import file

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,7 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-require("dotenv").config({ path: ".env" });
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
 
 const ALCHEMY_HTTP_URL = process.env.ALCHEMY_HTTP_URL || "";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
