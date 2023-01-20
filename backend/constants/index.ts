27b877d5667de6dadfc0a71e6c0de4de3eb52d47
megabyte0x <yashygkf@gmail.com>
2023-01-20 18:45:27 +0530

    completed test script for deployment

--- backend/constants/index.ts
+++ backend/constants/index.ts
@@ -1,9 +1,9 @@
-const { ethers } = require("hardhat");
+import { BigNumberish, BytesLike, ethers } from "ethers";
 
-const LINK_TOKEN = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB";
-const VRF_COORDINATOR = "0x8C7382F9D8f56b33781fE506E897a4F1e2d17255";
-const KEY_HASH =
+export const LINK_TOKEN: string = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB";
+export const VRF_COORDINATOR: string =
+  "0x8C7382F9D8f56b33781fE506E897a4F1e2d17255";
+export const KEY_HASH: BytesLike =
   "0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4";
-const FEE = ethers.utils.parseEther("0.0001");
-const abi: any[] = [];
-export default { LINK_TOKEN, VRF_COORDINATOR, KEY_HASH, FEE, abi };
+export const FEE: BigNumberish = ethers.utils.parseEther("0.0001");
+export const abi: any[] = [];
---
+++ backend/test/Probably.ts
@@ -0,0 +1,31 @@
+import { ethers } from "hardhat";
+import { LINK_TOKEN, KEY_HASH, FEE, VRF_COORDINATOR } from "../constants";
+
+describe("Probably", function () {
+  it("Should deploy Probably.sol and ProbablyNFT.sol", async () => {
+    const probably = await ethers.getContractFactory("Probably");
+    console.log(
+      VRF_COORDINATOR +
+        "-----" +
+        LINK_TOKEN +
+        "-------" +
+        KEY_HASH +
+        "---------" +
+        FEE
+    );
+    const probablyDeployed = await probably.deploy(
+      VRF_COORDINATOR,
+      LINK_TOKEN,
+      KEY_HASH,
+      FEE,
+      "uri/"
+    );
+    await probablyDeployed.deployed().catch((error: Error) => {
+      console.error(error);
+    });
+
+    const probablyAddress = probablyDeployed.address;
+
+    console.log(probablyAddress);
+  });
+});
